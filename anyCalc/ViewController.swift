d48ab1e8747d0dc2aca71da4923fc6f28d074716
JoeMartini <joe_martini@msn.com>
2015-07-06 23:08:46 +0800

    Initial commit
    wish to design a calculator universal and easy to use

---
+++ anyCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  anyCalc
+//
+//  Created by Martini Wang on 15/7/6.
+//  Copyright © 2015年 Martini Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
